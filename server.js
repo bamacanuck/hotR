327c98c3b0d4ba4fff05f6c719fce7b4bb8b0d4d
S.R. Scott <srandallscott@gmail.com>
2019-11-06 10:26:47 -0500

    body-parser...

--- server.js
+++ server.js
@@ -9,3 +9,17 @@ app.get('/', function (req, res) {
  
 app.listen(3000)
 
+
+// =============================================
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+ 
+app.use(function (req, res) {
+  res.setHeader('Content-Type', 'text/plain')
+  res.write('you posted:\n')
+  res.end(JSON.stringify(req.body, null, 2))
+})
