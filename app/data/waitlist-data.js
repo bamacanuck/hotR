d9e02526749acf0dfe682b3a575cfb1743cacacf
S.R. Scott <srandallscott@gmail.com>
2019-11-13 14:39:36 -0500

    building waitlist-data.js

---
+++ app/data/waitlist-data.js
@@ -0,0 +1,8 @@
+var waitingArray = [
+    {
+        customerName: "Tina",
+        customerEmail: "tmiller@millerco.net",
+        customerID: "tina01",
+        phoneNumber: "999-444-5555"
+    }
+];
