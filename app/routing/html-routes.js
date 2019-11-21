1baeb71e8b86e9c967d147dbbeae1aec7617cc89
S.R. Scott <srandallscott@gmail.com>
2019-11-21 13:14:31 -0500

    building html-routes.js... ...

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -6,11 +6,11 @@ module.exports = function (app) {
         res.sendFile(path.join(__dirname + '/tables.html'));
     });
 
-    app.get('/tables', function(req, res) {
-        res.sendFile(path.join(__dirname + '/tables.html'));
+    app.get('/reserve', function(req, res) {
+        res.sendFile(path.join(__dirname + '/reserve.html'));
     });
 
-    app.get('/tables', function(req, res) {
-        res.sendFile(path.join(__dirname + '/tables.html'));
+    app.get('/', function(req, res) {
+        res.sendFile(path.join(__dirname + '/home.html'));
     });
 }
