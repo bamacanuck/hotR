cd889c747e752701ca8b61ae2152f26c8ce8b7c1
S.R. Scott <srandallscott@gmail.com>
2020-01-30 12:09:37 -0500

    server file mods...

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const path = require ('path');
 const app = express();
 var PORT = process.env.PORT || 8080;
 
-app.get('/', function (req, res) {
-  res.send('server test success')
-});
+// app.get('/', function (req, res) {
+//   res.send('server test success')
+// });
 
 
 // =============================================
@@ -20,14 +20,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
  
 // parse application/json
 app.use(bodyParser.json())
- 
-app.use(function (req, res) {
-  res.setHeader('Content-Type', 'text/plain')
-  res.write('you posted:\n')
-  res.end(JSON.stringify(req.body, null, 2))
-});
 
-require('./app/routing/html-routes')(app);
+require('./app/routing/html-routes.js')(app);
 
 app.listen(PORT, function () {
     console.log("app \"listening\" on port: " + PORT);
