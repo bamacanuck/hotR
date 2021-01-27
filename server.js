67e58c2a9c01d0c1cbfcbd0dd3fe4a90982eb7af
Shane Scott <shaneradio@gmail.com>
2021-01-27 14:59:23 -0500

    redoing server file... ...

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const bodyParser = require('body-parser')
 const express = require('express')
+
 const app = express()
+const PORT = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
     res.send('hello, world')
 })
 
-app.listen(3000);
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+ 
+app.use(function (req, res) {
+  res.setHeader('Content-Type', 'text/plain')
+  res.write('you posted:\n')
+  res.end(JSON.stringify(req.body, null, 2))
+})
 
+app.listen(3000);
